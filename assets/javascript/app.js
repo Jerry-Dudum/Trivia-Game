5a9e8796d429269da25492d77cc3f3d2fd953023
Jerry Dudum <jsdudum@gmail.com>
2019-09-08 12:56:22 -0700

    added the questions to be asked with some variables below that will help create the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,100 @@
-var questions
+var questions = [
+    question1 = {
+        question: "What is Luffy made of?",
+        answer1: "Plastic",
+        answer2: "Rubber",
+        answer3: "Candy",
+        answer4: "Gum",
+        correct: "Rubber",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question2 = {
+        question: "How many swords does Zoro use?",
+        answer1: "1",
+        answer2: "2",
+        answer3: "3",
+        answer4: "4",
+        correct: "3",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question3 = {
+        question: "What is Nami an expert in?",
+        answer1: "Navigation",
+        answer2: "Repairing",
+        answer3: "Shooting",
+        answer4: "Fighting",
+        correct: "Navigation",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question4 = {
+        question: "What weapon does Usopp wield?",
+        answer1: "Gun",
+        answer2: "Sword",
+        answer3: "He doesn't use weapons",
+        answer4: "Slingshot",
+        correct: "Slingshot",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question5 = {
+        question: "Sanji is the ____ for the Straw Hat Pirates.",
+        answer1: "Captain",
+        answer2: "Musician",
+        answer3: "Chef",
+        answer4: "Deckhand",
+        correct: "Chef",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question6 = {
+        question: "Robin's devil fruit ability is called?",
+        answer1: "Hana Hana no Mi",
+        answer2: "Desu Desu no Mi",
+        answer3: "Gomu Gomu no Mi",
+        answer4: "Hito Hito no Mi",
+        correct: "Hana Hana no Mi",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question7 = {
+        question: "What is so special about Franky?",
+        answer1: "He's half man, half demon",
+        answer2: "He's a Fishman",
+        answer3: "He's half tiger, half man",
+        answer4: "He's a cyborg",
+        correct: "He's a cyborg",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question8 = {
+        question: "Brook's devil fruit changed his appearance to?",
+        answer1: "A skeleton",
+        answer2: "A child",
+        answer3: "An old man",
+        answer4: "A ghost",
+        correct: "A skeleton",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question9 = {
+        question: "Chopper is a what?",
+        answer1: "Dog",
+        answer2: "Motorcycle",
+        answer3: "Reindeer",
+        answer4: "Cat",
+        correct: "Reindeer",
+        image: "FILL WITH IMAGE LATER"
+    },
+    question10 = {
+        question: "Jinbei know's what special martial arts technique?",
+        answer1: "Water Jutsu",
+        answer2: "Fishman Karate",
+        answer3: "Swan Technique",
+        answer4: "Devil Trigger",
+        correct: "Fishman Karate",
+        image: "FILL WITH IMAGE LATER"
+    }
+];
+
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+var timer = 5;
+var timerID;
+var timeUpID;
+var questionIndex = 0;
